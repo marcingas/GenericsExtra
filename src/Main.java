88cdc7fbec65285960860eef529e3a7610db4fa9
Marcin <gasiorek.mail@gmail.com>
2023-04-04 09:39:30 +0200

    <?>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +9,8 @@ public class Main {
         for(int i = 0; i < studentCount; i++){
             students.add(new Student());
         }
-        printStudent(students);
+//        printStudent(students);
+        printMoreLists(students);
 
 
         List<UJStudent> ujStudents = new ArrayList<>();
@@ -18,11 +20,44 @@ public class Main {
 //        although UJStudent extend Student so we can say its the same, when used as
 //        a reference types, a container of one type has no relationship to the same container of another type
         System.out.println("____UJSTUDENT LIST______");
-        printStudent(ujStudents);
+//        printStudent(ujStudents);
+        printMoreLists(ujStudents);
+        System.out.println("test list: ");
+        testList(new ArrayList<String>(List.of("John", "Poul")));
+        testList(new ArrayList<Integer>(List.of(1, 2)));
+        testList(new ArrayList<>(List.of(1, "john")));
     }
-    public static <T> void printStudent(List<T> students){
+
+    public static void testList(List<?>list){
+        for (var element: list){
+            if(element instanceof String s){
+                System.out.println("String: " + s.toUpperCase());
+            } else if (element instanceof  Integer i) {
+                System.out.println("Integer: " + i.floatValue());
+
+            }
+        }
+    }
+//    Abovwe method can solve problem with those two methods:
+//    public static void testList(List<String>list){
+//        for (var element: list){
+//            System.out.println("String: " + element.toUpperCase());
+//        }
+//    }
+//    public static void testList(List<Integer>list){
+//        for (var element: list){
+//            System.out.println("String: " + element.floatValue());
+//        }
+//    }
+    public static  void printMoreLists(List<? extends Student> students){
         for(var student: students){
-            System.out.println(student);
+            System.out.println(student.getYearStared() + ": " + student);
+        }
+        System.out.println();
+    }
+    public static <T extends Student> void printStudent(List<T> students){
+        for(var student: students){
+            System.out.println(student.getYearStared() + ": " + student);
         }
         System.out.println();
     }
