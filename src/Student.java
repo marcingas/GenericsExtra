a9f978b24ad34aeef972982b01cd8313189b188c
Marcin <gasiorek.mail@gmail.com>
2023-04-04 08:48:48 +0200

    generate code for creating studenst

--- src/Student.java
+++ src/Student.java
@@ -7,5 +7,21 @@ public class Student {
     protected static Random random = new Random();
     private static String[] firstNames = {"Ann", "Bill","Cathy","John","Tim" };
     private static String[] courses = {"Java","C++","Python"};
-    
+
+    @Override
+    public String toString() {
+        return "%-15s %-15s %d".formatted(name,course,yearStared);
+    }
+
+    public int getYearStared() {
+        return yearStared;
+    }
+
+    public Student(){
+        int lastNameIndex = random.nextInt(65,91);
+        name = firstNames[random.nextInt(5)] + " " + (char) lastNameIndex;
+        course = courses[random.nextInt(3)];
+        yearStared = random.nextInt(2018, 2023);
+
+    }
 }
