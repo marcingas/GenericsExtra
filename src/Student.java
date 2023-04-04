8f0e8b3efd3283ad292283f2aa8debb855f96f22
Marcin <gasiorek.mail@gmail.com>
2023-04-04 08:43:17 +0200

    gitignore

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,11 @@
+import java.util.Random;
+
 public class Student {
     private String name;
     private String course;
     private int yearStared;
+    protected static Random random = new Random();
+    private static String[] firstNames = {"Ann", "Bill","Cathy","John","Tim" };
+    private static String[] courses = {"Java","C++","Python"};
+    
 }
